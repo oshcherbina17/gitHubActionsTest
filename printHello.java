7ca561b023cc2d79c7448321229309554b36cc16
oshcherbina17 <110410608+oshcherbina17@users.noreply.github.com>
2023-06-02 15:01:42 +0300

    Update printHello.java

--- printHello.java
+++ printHello.java
@@ -5,7 +5,7 @@ import java.lang.invoke.MethodHandles;
 class HelloWorld {
   
     public static void main(String[] args) {
-        private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+        final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
         System.out.println("Hello, World!"); 
         LOGGER.info("test");
