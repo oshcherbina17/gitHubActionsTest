b5a2a2b3b6531de7b9bbf23eb9968597f73c6717
oshcherbina17 <110410608+oshcherbina17@users.noreply.github.com>
2023-06-02 16:26:40 +0300

    Update printHello.java

--- printHello.java
+++ printHello.java
@@ -1,12 +1,6 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.lang.invoke.MethodHandles;
-
 class HelloWorld {
   
     public static void main(String[] args) {
-        final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
         System.out.println("Hello, World!"); 
         LOGGER.info("test");
     }
