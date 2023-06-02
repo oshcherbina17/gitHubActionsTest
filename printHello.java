6705166a1e2327b31b56e343267adae0a8828cb2
oshcherbina17 <110410608+oshcherbina17@users.noreply.github.com>
2023-06-02 14:55:11 +0300

    Update printHello.java

--- printHello.java
+++ printHello.java
@@ -1,5 +1,9 @@
 class HelloWorld {
+  
     public static void main(String[] args) {
+        private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
         System.out.println("Hello, World!"); 
+        LOGGER.info("test");
     }
 }
